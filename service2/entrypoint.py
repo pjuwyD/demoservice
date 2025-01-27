53cca9014abfd178926799f8875ea5651d000bf8
Dario Pevec <pevec@axiros.com>
2025-01-27 13:31:49 +0100

    test - debug

--- service2/entrypoint.py
+++ service2/entrypoint.py
@@ -7,6 +7,5 @@ SERVICE1_URL = os.getenv("SERVICE1_URL", "http://service1:8080")
 message = requests.get(sys.stdin.readline()).text
 data = ["md5", message]
 
-print(f"Sending request to: {SERVICE1_URL}")
 res = requests.post(SERVICE1_URL, data="\n".join(data))
 print(res.text)
